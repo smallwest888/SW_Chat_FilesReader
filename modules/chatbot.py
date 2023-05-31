942e54066b6a8c30e1eed2c3be0b6337b7549429
smallwest888 <63802737+smallwest888@users.noreply.github.com>
2023-05-31 20:14:11 +0200

    Update chatbot.py

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -19,7 +19,6 @@ class Chatbot:
         If you don't know the answer, just say you don't know. Do NOT try to make up an answer.
         If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
         Use as much detail as possible when responding.
-        Answer questions using the language of the document.
 
         context: {context}
         =========
